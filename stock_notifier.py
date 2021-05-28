87eb824b9e23f2bc57fa51751cea9fd79ea511ea
jan <jan.capasso@students.ffhs.ch>
2021-05-28 12:59:32 +0200

    refactor config

--- stock_notifier.py
+++ stock_notifier.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 from telegram_bot import TelegramBot
-from ymlconfig import YMLConfig
+from yml_config import YMLConfig
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
@@ -15,20 +15,20 @@ class StockNotifier:
         try:
             self.config = YMLConfig().config
             self.telegram_bot = TelegramBot(self.config)
-            [self.check_inventories(page) for page in self.config['pages']]
+            [self.check_inventories(page, url) for page in self.config['pages'] for url in page['urls']]
         except Exception as e:
             print(e)
 
-    def check_inventories(self, page):
-        response = requests.get(page['url'], headers=HEADERS)
+    def check_inventories(self, page, url):
+        response = requests.get(url, headers=HEADERS)
 
         if response.status_code != 200:
-            self.telegram_bot.page_error(page['url'], page.status_code)
+            self.telegram_bot.page_error(url, page.status_code)
         else:
             if self.is_in_stock(response.content, page):
-                self.telegram_bot.in_stock(page['url'])
+                self.telegram_bot.in_stock(url)
             else:
-                print('Out of Stock: {}'.format(page['url']))
+                print('Out of Stock: {}'.format(url))
 
     def is_in_stock(self, html, page) -> bool:
         soup = BeautifulSoup(html, 'html.parser')
